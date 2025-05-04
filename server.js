e699c4de7c6ed16131e8b5df82ac0a98a87712a7
KrukX_rn50@student.itstep.org <KrukX_rn50@student.itstep.org>
2025-05-04 23:46:45 +0300

    FINAL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const multer = require("multer");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
